4e9e6a7e81eed0b01d8f1cb37e81d6c203f55a41
mattudry <jazumazu17@gmail.com>
2021-05-13 12:26:45 -0400

    updated hwk4 to hwk5

--- players.py
+++ players.py
@@ -1,5 +1,5 @@
 # Christie Yu, Matt Udry
-# CPSC 327 (Object Oriented Programming) Homework 4
+# CPSC 327 (Object Oriented Programming) Homework 5
 
 class PlayerState:
     """State object used to toggle between WhiteState and BlackState."""
